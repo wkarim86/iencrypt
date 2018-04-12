2bc9ad2306299d4e69cac95161b454c62d540fbf
wkarim86 <wkarim86@gmail.com>
2018-04-12 18:13:10 +0500

    Encryption Utility for mac

---
+++ iEncrypt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iEncrypt
+//
+//  Created by Waqas Karim on 12/04/2018.
+//  Copyright © 2018 Waqas Karim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iEncrypt/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  iEncrypt
+//
+//  Created by Waqas Karim on 12/04/2018.
+//  Copyright © 2018 Waqas Karim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var scrollView: NSScrollView!
+    @IBOutlet weak var encryptBtn: NSButton!
+    @IBOutlet weak var decryptBtn: NSButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+    
+    func encryptMe(){
+        
+    }
+
+
+}
+
